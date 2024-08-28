d530adf03a4baed0512eb9fb74c83b122478afbc
Csongor Szepesvari <csongor.szepesvari@gmail.com>
2024-08-28 13:35:03 -0600

    started refactoring code to make it more scalable and clean

---
+++ helpers/environment_variables.py
@@ -0,0 +1,28 @@
+
+class Player():
+    def __init__(self, win_chance):
+        self.strategy = {"Q1":0, "Q2":0, "Q3":0, "Q4":0}
+        self.win_chance = win_chance
+        
+class Category():
+    def __init__(self,mean,std,size):
+        self.mean = mean
+        self.std = std
+        self.size = size
+    
+    def get_sample()
+
+
+class Game():
+    def __init__(self, num_candidates:int, category_sizes:list, eval_type:function, win_chances: list):
+        self.players = [Player(win_chances[i]) for i in range(num_candidates)]
+
+
+    def simulate_game(strategies : dict, num_candidates: int, sizes: list, eval_type: function, break_vals: list) -> dict:
+        '''
+        Simulate game takes all of the variables which define an admissions game environment and simulates the actual outcomes.
+        It returns a dictionary of the utilities achieved by each player
+        '''
+
+
+def utility_eval
