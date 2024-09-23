b110d0247c6f55948ccf66af3a73a1cb10b617ba
Csongor-Szepesvari <csongor.szepesvari@gmail.com>
2024-09-23 14:42:53 -0600

    added sum loss calculation

--- helpers/object_oriented_refactor/Game.py
+++ helpers/object_oriented_refactor/Game.py
@@ -7,8 +7,149 @@ A list of different categories - what players draw their action sets from.
 The game-mode, also known as the utility function, that players will optimize for in consideration of other players.
 '''
 from Category import Category
-from Player import Player
+
 import numpy as np
+
+class Player():
+    def __init__(self, win_value:float, blind:bool, categories:list[Category]):
+        self.strategy = {}
+        for category in categories:
+            self.strategy[category.get_name()] = 0 # a floating point representation of the percentage of students to admit from a particular category
+        self.win_value = win_value
+        self.blind = blind
+
+    def calculate_win_chance(self, players):
+        win_percent = self.win_value / (sum([player.get_win_value() for player in players]))
+        return win_percent
+    
+    def set_strategy(self, game, strat_numbers_list:list):
+        assert len(strat_numbers_list) == len(game.categories)
+        assert max(strat_numbers_list) <= 1
+        assert min(strat_numbers_list) >= 0
+        assert type(game) == Game
+
+        for i in range(len(strat_numbers_list)):
+            category = game.categories[i]
+            strategy = strat_numbers_list[i]
+            name = category.get_name()
+            self.strategy[name] = strategy
+
+    def get_win_value(self):
+        return self.win_value
+    
+    def calc_expected_attendees(self, strategy, game):
+        '''
+        This method calculates the expected attendees from a particular strategy in context of all other player's strategies
+        '''
+        categories = game.categories
+        other_players = [player for player in game.players if player != self]
+        print(other_players)
+        for category in categories:
+            pass
+
+    
+    def getSumLost(self, other_players, category):
+
+        def helper(occupancy_list, index):
+
+            _occ = occupancy_list
+            print(_occ)
+            # start adding up the probabilities of the event
+            prob_event = 1
+            # include things in the denominator
+            denom = self.win_value
+
+            # check if we're at the bottom
+            if index == len(_occ)-1:
+                # calculate the value of this event
+                for i in range(len(_occ)):
+                    occupies = _occ[i]
+                    if occupies:
+                        prob_event *= other_players[i].strategy[category]
+                        denom += other_players[i].win_value
+                    else:
+                        prob_event *= (1-other_players[i].strategy[category])
+                # actual value calculation for a loss
+                return prob_event * (1 - self.win_value/denom)
+
+            else:
+                _index = index + 1
+                false_val = helper(occupancy_list=_occ, index=_index)
+                _occ[_index] = True
+                true_val = helper(occupancy_list=_occ, index=_index)
+                return false_val + true_val
+
+        f_start = [False for i in range(len(other_players))]
+        print(f_start)
+        f_val = helper(f_start, 0)
+        print(f_val)
+        f_start[0] = True
+        t_val = helper(f_start, 0)
+
+        return f_val + t_val
+
+    def find_feasible_limits(self, game)->dict[str:float]:
+        '''
+        Projects a desired strategy out to the realisable game space based on other's strategies.
+        Inputs:
+
+            game->Game : the game object that contains the categories and other player's strategies so that we can find our feasible space.
+
+        Outputs:
+            max_category_values:dict[str:float] : a dictionary representing the maximum values for each category
+        '''
+        assert type(game) == Game
+        return_dict = {}
+        for category in game.categories:
+            return_dict[category.get_name()] = 1
+
+    def best_response(self, game):
+        assert type(game) == Game
+        '''
+        This method calculates the best response in response to a game class and updates the strategy based on the best response type
+        '''
+        category_sizes = [category.get_size() for category in game.categories]
+        max_admit = game.to_admit
+
+        
+        if game.game_mode_type == "expected":
+            # use the expected case algorithm to evaluate what the expected best response would be in this setting
+            
+            # what are we trying to maximize in this setting?
+            # c(game) = sum(attendees) - (num_attendees-desired_attendees)^2
+            # first term wants you to maximize the number of students you admit, the second term caps it
+
+            # in order to maximize this, we look at other players' allocation or occupancy in each category and calculate how many admissions to give out in order of best to worst
+            
+
+            # follow the simple logic of increasing admittances to Q1 > Q2 > Q3 > Q4 etc
+            if self.blind:
+                # if our player is blind then we just have Q1 + Q2 > Q3 + Q4
+                
+                # calculate our target distribution first and then scale it based on calc_collisions
+                # our target is to have max_admit
+                combined_high_size = sum(category_sizes[:2])
+                combined_low_size = sum(category_sizes[2:])
+
+                high_admissions = min(combined_high_size, max_admit)
+                low_admissions = min(combined_low_size, max_admit-high_admissions)
+
+                desired_strategy = [high_admissions/combined_high_size, low_admissions/combined_low_size]
+                self.project_strategy(self, desired_strategy, game)
+
+        elif game.game_mode_type == "top_k":
+            # use the top k algorithm to evaluate what the expected best response would be
+            # c(game) = sum(top_k) - (num_attendees-desired_attendees)^2
+            # first term wants you to maximize the number of students you admit, the second term caps it
+
+            # in order to maximize this we keep building up students by student until the marginal value gained from having additional students is less than the chance of finding someone better
+            # this is a greedy method
+
+            # the perfect method is to literally find
+            pass
+
+
+
 
 class Game():
     '''
@@ -83,101 +224,6 @@ class Game():
 
 
 
-class Player():
-    def __init__(self, win_value:float, blind:bool, categories:list[Category]):
-        self.strategy = {}
-        for category in categories:
-            self.strategy[category.get_name()] = 0 # a floating point representation of the percentage of students to admit from a particular category
-        self.win_value = win_value
-        self.blind = blind
-
-    def calculate_win_chance(self, players):
-        win_percent = self.win_value / (sum([player.get_win_value() for player in players]))
-        return win_percent
-    
-    def set_strategy(self, game:Game, strat_numbers_list:list):
-        assert len(strat_numbers_list) == len(game.categories)
-        assert max(strat_numbers_list) <= 1
-        assert min(strat_numbers_list) >= 0
-
-        for i in range(len(strat_numbers_list)):
-            category = game.categories[i]
-            strategy = strat_numbers_list[i]
-            name = category.get_name()
-            self.strategy[name] = strategy
-
-    def get_win_value(self):
-        return self.win_value
-    
-    def calc_expected_attendees(self, strategy, game):
-        '''
-        This method calculates the expected attendees from a particular strategy in context of all other player's strategies
-        '''
-        categories = game.categories
-        other_players = [player for player in game.players if player != self]
-        print(other_players)
-        for category in categories:
-            pass
-
-
-    def find_feasible_limits(self, game:Game)->dict[str:float]:
-        '''
-        Projects a desired strategy out to the realisable game space based on other's strategies.
-        Inputs:
-
-            game->Game : the game object that contains the categories and other player's strategies so that we can find our feasible space.
-
-        Outputs:
-            max_category_values:dict[str:float] : a dictionary representing the maximum values for each category
-        '''
-        return_dict = {}
-        for category in game.categories:
-            return_dict[category.get_name()] = 1
-
-    def best_response(self, game:Game):
-        '''
-        This method calculates the best response in response to a game class and updates the strategy based on the best response type
-        '''
-        category_sizes = [category.get_size() for category in game.categories]
-        max_admit = game.to_admit
-
-        
-        if game.game_mode_type == "expected":
-            # use the expected case algorithm to evaluate what the expected best response would be in this setting
-            
-            # what are we trying to maximize in this setting?
-            # c(game) = sum(attendees) - (num_attendees-desired_attendees)^2
-            # first term wants you to maximize the number of students you admit, the second term caps it
-
-            # in order to maximize this, we look at other players' allocation or occupancy in each category and calculate how many admissions to give out in order of best to worst
-            
-
-            # follow the simple logic of increasing admittances to Q1 > Q2 > Q3 > Q4 etc
-            if self.blind:
-                # if our player is blind then we just have Q1 + Q2 > Q3 + Q4
-                
-                # calculate our target distribution first and then scale it based on calc_collisions
-                # our target is to have max_admit
-                combined_high_size = sum(category_sizes[:2])
-                combined_low_size = sum(category_sizes[2:])
-
-                high_admissions = min(combined_high_size, max_admit)
-                low_admissions = min(combined_low_size, max_admit-high_admissions)
-
-                desired_strategy = [high_admissions/combined_high_size, low_admissions/combined_low_size]
-                self.project_strategy(self, desired_strategy, game)
-
-        elif game.game_mode_type == "top_k":
-            # use the top k algorithm to evaluate what the expected best response would be
-            # c(game) = sum(top_k) - (num_attendees-desired_attendees)^2
-            # first term wants you to maximize the number of students you admit, the second term caps it
-
-            # in order to maximize this we keep building up students by student until the marginal value gained from having additional students is less than the chance of finding someone better
-            # this is a greedy method
-
-            # the perfect method is to literally find
-            pass
-
 
                 
 
@@ -194,13 +240,16 @@ player2 = Player(40, True, categories)
 player3 = Player(50, True, categories)
 game = Game(3, 8, [player, player2, player3], categories, game_mode_type="expected")
 
-player.calc_expected_attendees({"Q1":1, "Q2":1}, game=game)
+#player.calc_expected_attendees({"Q1":1, "Q2":1}, game=game)
+
+player.getSumLost(other_players=[player2, player3], category="Q1")
 
 
 
-
+'''
 game = Game(players=[Player(60), Player(40)], categories=[Category(size=12, std=1, mean=2, name="Q1"), Category(size=24, std=2, mean=1, name="Q2")])
 for player in game.players:
     player.set_strategy(game, [0.5, 0.6])
 
 game.simulate_game()
+'''
